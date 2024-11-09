befe0a2781b33e86833b8eed7c9339ab6a56a3a6
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2024-11-09 10:57:24 +0530

    Update 3d-card.tsx

--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { cn } from "@/utils/cn";
-import Image from "next/image";
 import React, {
   createContext,
   useState,
@@ -10,10 +9,12 @@ import React, {
   useEffect,
 } from "react";
 
+// Context definition
 const MouseEnterContext = createContext<
   [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
 >(undefined);
 
+// CardContainer Component
 export const CardContainer = ({
   children,
   className,
@@ -35,16 +36,16 @@ export const CardContainer = ({
     containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
   };
 
-  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseEnter = () => {
     setIsMouseEntered(true);
-    if (!containerRef.current) return;
   };
 
-  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseLeave = () => {
     if (!containerRef.current) return;
     setIsMouseEntered(false);
     containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
   };
+
   return (
     <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
       <div
@@ -76,6 +77,7 @@ export const CardContainer = ({
   );
 };
 
+// CardBody Component
 export const CardBody = ({
   children,
   className,
@@ -95,6 +97,7 @@ export const CardBody = ({
   );
 };
 
+// CardItem Component
 export const CardItem = ({
   as: Tag = "div",
   children,
@@ -116,7 +119,7 @@ export const CardItem = ({
   rotateX?: number | string;
   rotateY?: number | string;
   rotateZ?: number | string;
-  [key: string]: any;
+  [key: string]: unknown;
 }) => {
   const ref = useRef<HTMLDivElement>(null);
   const [isMouseEntered] = useMouseEnter();
@@ -145,7 +148,7 @@ export const CardItem = ({
   );
 };
 
-// Create a hook to use the context
+// Hook to use the MouseEnter context
 export const useMouseEnter = () => {
   const context = useContext(MouseEnterContext);
   if (context === undefined) {
